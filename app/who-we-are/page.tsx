67f456604a2386970ea1e953e4aacc38bfd130a1
mellinslab <164794786+mellinslab@users.noreply.github.com>
2024-03-24 13:19:40 -0700

    Update page.tsx

--- app/who-we-are/page.tsx
+++ app/who-we-are/page.tsx
@@ -8,10 +8,65 @@ export const metadata: Metadata = {
 
 const examplePeople = [
   {
-    name: 'Elizabeth D. Mellins,MD',
+    name: 'Elizabeth D. Mellins, MD',
     title: 'Principal Investigator',
     image: 'Elizabeth.jpg',
     email: 'mellins@stanford.edu',
+  },
+  {
+    name: 'Claudia Macaubas, PhD',
+    title: 'Research Scientist',
+    image: 'Claudia.jpg',
+    email: 'macaubas@stanford.edu',
+  },
+  {
+    name: 'Batuhan Bayram, MD',
+    title: 'Postdoctoral Fellow',
+    image: 'Batuhan.jpg',
+    email: 'bayramb@stanford.edu',
+    linkedin: 'https://www.linkedin.com/in/batubayrm/',
+  },
+  {
+    name: 'Noor Hussein,PhD',
+    title: 'Postdoctoral Fellow',
+    image: 'Noor.jpg',
+    email: 'nhussei@stanford.edu',
+  },
+  {
+    name: 'Ayan Mondal, PhD',
+    title: 'Postdoctoral Fellow',
+    image: 'Ayan.jpg',
+    email: 'ayanm@stanford.edu',
+  },
+  {
+    name: 'Agnieszka Kalinowski, MD,PhD',
+    title: 'Clinical Assistant Professor',
+    image: 'Agnes.jpg',
+    email: 'akalinow@stanford.edu',
+  },
+  {
+    name: 'Xiaowen Ding , PhD',
+    title: 'Postdoctoral Fellow',
+    image: 'Xiaowen.jpg',
+    email: 'xwd15279@stanford.edu',
+  },
+  {
+    name: 'Samira Galehdari, MS',
+    title: 'Research Assistant',
+    image: 'Samira.jpg',
+    email: 'samigal@stanford.edu',
+  },
+  {
+    name: 'Paulette Danahe Monroy Alfaro',
+    title: 'REACH Post-baccalaureate Scholar ',
+    image: 'Paulette.jpg',
+    email: 'pmonroy@stanford.edu',
+  },
+  {
+    name: 'Nelia Lechuga, BS',
+    title: 'Undergraduate student',
+    image: 'Nelia.jpg',
+    email: 'nlechuga@stanford.edu',
   },
 ];
 
