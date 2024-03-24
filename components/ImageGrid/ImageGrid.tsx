691007b5768e963994ea0ed4f10e58a22daa2027
mellinslab <164794786+mellinslab@users.noreply.github.com>
2024-03-24 14:14:47 -0700

    Update ImageGrid.tsx

--- components/ImageGrid/ImageGrid.tsx
+++ components/ImageGrid/ImageGrid.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import { LayoutGrid } from "../ui/layout-grid";
 
 export function ImageGrid({ cards }: { cards: any[]; }) {
-  const LabImageComponent = (image: any, title: any) => (
+  const LabImageComponent = ({ image, title }: { image: any; title: any; }) => {
+    return (
     <div className="flex flex-col items-center justify-center space-y-4 border p-2 rounded">
       <img src={image} alt={title} />
       <p>{title}</p>
     </div>
-  );
+  )
+};
 
   return (
     <div className="flex flex-col items-center justify-center space-y-8 py-8 w-full">
