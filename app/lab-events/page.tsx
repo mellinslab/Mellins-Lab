5daaa41f6099ddc448215298010889e8ca1ca35d
mellinslab <164794786+mellinslab@users.noreply.github.com>
2024-03-24 13:24:31 -0700

    Update page.tsx

--- app/lab-events/page.tsx
+++ app/lab-events/page.tsx
@@ -7,14 +7,18 @@ export const metadata: Metadata = {
 };
 
 export default function LabEvents() {
-  const ImageSkeleton = ({ title, description }: { title: string; description: string; }) => {
+  const ImageSkeleton = ({ title, description }: { title: string; description?: string; }) => {
     return (
       <div>
         <p className="font-bold text-4xl text-white">{title}</p>
-        <p className="font-normal text-base text-white"></p>
-        <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-          {description}
-        </p>
+        {description && (
+          <>
+            <p className="font-normal text-base text-white"></p>
+            <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+              {description}
+            </p>
+          </>
+        )}
       </div>
     );
   };
