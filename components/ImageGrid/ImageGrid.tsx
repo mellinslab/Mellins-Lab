5cdbf40ff3f2511574177c6a45726adbb7f385ee
mellinslab <164794786+mellinslab@users.noreply.github.com>
2024-03-24 14:05:19 -0700

    Update ImageGrid.tsx

--- components/ImageGrid/ImageGrid.tsx
+++ components/ImageGrid/ImageGrid.tsx
@@ -3,18 +3,17 @@ import React from "react";
 import { LayoutGrid } from "../ui/layout-grid";
 
 export function ImageGrid({ cards }: { cards: any[]; }) {
-  const splitCards = cards.reduce((acc, card, i) => {
-    if (i % 4 === 0) {
-      acc.push([]);
-    }
-    acc[acc.length - 1].push(card);
-    return acc;
-  }, []);
+  const LabImageComponent = (image: string, title: string) => (
+    <div className="flex flex-col items-center justify-center space-y-4 border p-2 rounded">
+      <img src={image} alt={title} />
+      <p>{title}</p>
+    </div>
+  );
 
   return (
-    <div className="h-screen py-8 w-full">
-      {splitCards.map((cards: any[], i: number) => (
-        <LayoutGrid key={i} cards={cards} />
+    <div className="flex flex-col items-center justify-center space-y-8 py-8 w-full">
+      {cards.map((card) => (
+        <LabImageComponent image={card.image} title={card.title} />
       ))}
     </div>
   );
