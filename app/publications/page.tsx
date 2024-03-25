d3a6185dfaaa82edf67e208bb19467aec14cebb4
mellinslab <164794786+mellinslab@users.noreply.github.com>
2024-03-24 22:06:05 -0700

    Update page.tsx

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -14,13 +14,49 @@ const publications = [
     pubmed: "30926644",
   },
   {
-    title: "Gene Editing in Rare Genetic Disorders: CRISPR-Cas9 Approaches",
-    authors: "Johnson L., O'Reilly M., Singh V.",
-    published: 2022,
-    journal: "Advances in Genetic Engineering",
-    pubmed: "",
+    title: "Human intestinal enteroids model MHC-II in the gut epithelium",
+    authors: "2.	Wosen JE, Ilstad-Minnihan A, Co JY, Jiang W, Mukhopadhyay D, Fernandez-Becker N, Kuo CJ, Amieva MR, Mellins ED",
+    published: 2019,
+    journal: "Front. Immunol",
+    pubmed: "31481960",
+  },
+  {
+    title: "Synergy between B cell receptor/antigen uptake and MHCII peptide editing relies on HLA-DO tuning",
+    authors: "3.	Jiang W, Adler LN, Macmillan H, Mellins ED",
+    published: 2019,
+    journal: "Sci Rep.",
+    pubmed: "31554902",
+  },
+  {
+    title: "An emergent high fatality lung disease in systemic juvenile idiopathic arthritis",
+    authors: "4.	Saper VE, Chen G, Deutsch GH, … Mellins ED",
+    published: 2019,
+    journal: "Ann. Rheum.",
+    pubmed: "31562126",
+  },
+  {
+    title: "Hypocretin-specific CD4+ T cells in narcolepsy patients and controls: in vivo clonal expansion and phenotype",
+    authors: "5.	Jiang W, Birtley J, Hung S-C, Wang W, Macaubas C, Kornum B, Chiou S-H, Tian L, Huang H, Adler L, Weaver G, Lu L, Ilstad-Minnihan A, Somasundaram S, Ayyangar S, Davis MM, Stern LJ, Mellins EDD",
+    published: 2019,
+    journal: "Nat. Comm.",
+    pubmed: "31562126",
+  },
+  {
+    title: "Allergen activation preferentially drives monocyte differentiation into dendritic cells in allergic vs healthy individuals through IL-4/IL-13 pathways. ",
+    authors: "6.	Zhou X, Yu W, Lyu S-C, Macaubas C, Bunning B, Mellins ED*, Nadeau KC",
+    published: 2021,
+    journal: " J Exp Med.",
+    pubmed: "33944900",
+  },
+  {
+    title: "Neutrophil serine proteases link histone proteolytic cleavage to macrophage differentiation and sJIA. ",
+    authors: "7.	Cheung P, Schaffert S, Chang SE, Dvorak M, Donato M, Macaubas C, Li T-M, Foecke MH, Zhang L, Schulert GS, Nigrovic P, Henderson L, Elias JE, Gozani O, Mellins ED*, Khatri P*, Utz PJ*, Kuo AJ",
+    published: 2021,
+    journal: " Nat Immunol.",
+    pubmed: "3401712",
   },
 ];
+
 
 export default function Publications() {
   return (
